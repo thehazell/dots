45c0265543efab00ca84590342a8cf67723c7253
Hazel <taco.xr@proton.me>
2026-07-14 18:32:37 -0500

    move the shell autostart above vesktop and remove QPA platform theme override, this is set by hl.env in a following commit

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -3,11 +3,12 @@
 local config = require("config")
 
 hl.on("hyprland.start", function()
+    -- kind of a dirty fix(?) for caelestia
+    -- not loading some icons from theme
+    hl.exec_cmd(config.shell)
+
     -- starts vesktop on hyprland start
     -- take this as an example on how to autostart apps
     hl.exec_cmd(config.vesktop)
 
-    -- kind of a dirty fix(?) for caelestia
-    -- not loading some icons from theme
-    hl.exec_cmd("QT_QPA_PLATFORMTHEME=gtk3 " .. config.shell)
  end)
