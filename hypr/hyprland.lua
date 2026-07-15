04da04da0cf7641864eb04d745abcb9a723ef0bb
Hazel <taco.xr@proton.me>
2026-07-14 22:19:15 -0500

    add hyprquickshot submodule, add some QT vars, upload Hyprland color scheme

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -22,5 +22,6 @@ require("window_rules.no_maximize")
 -- i just do vars here because im lazy
 hl.env("XCURSOR_SIZE", config.xcursor_size)
 hl.env("HYPRCURSOR_SIZE", config.hyprcursor_size)
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
+hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
-hl.env("QT_STYLE_OVERRIDE", "kvantum")
