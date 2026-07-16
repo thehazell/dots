fc12c9600ce804cd1e4305e55ae51f1ba305fb19
thehazell <taco.xr@proton.me>
2026-07-15 23:25:07 -0500

    autostart music app, adjust comments and formatting

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -5,13 +5,17 @@ local config = require("config")
 hl.on("hyprland.start", function()
     hl.exec_cmd(config.shell)
 
-    -- starts vesktop on hyprland start
-    -- take this as an example on how to autostart apps
+    -- start vesktop on default workspace
     hl.exec_cmd(config.vesktop)
+
+    -- start spotify on workspace 2 without focusing it
+    hl.exec_cmd(config.music, { workspace = "2 silent" })
 
     -- NOTE
     -- for this to work correctly, you must have already setup the source properly
     -- the easiest one by far is screen capture (pipewire)
     -- if you experience issues such as incorrect window recorded, or nothing at all, check your source settings
-    hl.exec_cmd(config.obs , {workspace = "special:" .. config.obs_special_workspace_name .. " silent"})
+    -- this starts obs in a special workspace defined by config.obs_special_workspace_name without focusing it
+    hl.exec_cmd(config.obs, { workspace = "special:" .. config.obs_special_workspace_name .. " silent" })
+
  end)
