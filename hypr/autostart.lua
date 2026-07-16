75cdae155fd59b7055465d8b314a1f9747754261
thehazell <taco.xr@proton.me>
2026-07-15 22:34:00 -0500

    autostart obs in the special workspace defined by config.obs_special_workspace_name

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -13,6 +13,5 @@ hl.on("hyprland.start", function()
     -- for this to work correctly, you must have already setup the source properly
     -- the easiest one by far is screen capture (pipewire)
     -- if you experience issues such as incorrect window recorded, or nothing at all, check your source settings
-    hl.exec_cmd(config.obs)
-
+    hl.exec_cmd(config.obs , {workspace = "special:" .. config.obs_special_workspace_name .. " silent"})
  end)
